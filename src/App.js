61ce94ef86fbc65476b424966f0b2690a5dc850b
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-03-25 17:34:32 +0530

    MUI V5: The SX Prop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import {styled} from '@mui/material/styles';
+import { hover } from '@testing-library/user-event/dist/hover';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     background:theme.palette.secondary.main,
   }));
 
+  const test=false;
   return (
     <div className="App">
       <Typography component="div" variant='mytypography'>React Mui5</Typography>
@@ -19,7 +21,31 @@ function App() {
       <Typography sx={{ color: 'mycustomeColor.customdark',p:1 }}>Cusom Color Apply</Typography>
       <Typography sx={{ color: 'mycustomeColor.customLight',m:2 }}>Cusom Color Apply</Typography>
       <MyCustomButton variant='contained'>Custom Button</MyCustomButton>
+      <Box
+        sx={{
+          width: 300,
+          height: 300,
+          backgroundColor: 'primary.main',
+          '&:hover': {
+            backgroundColor: 'secondary.main',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      />
+      <Button sx={[{
+        width:{xs:100,sm:200,md:300,lg:400,xl:500},
+        border:4,
+        borderColor:'secondary.main',
+        '&.MuiButton-root':{
+          height:'100px',
+        },
 
+      },
+      test &&{
+        border:6,
+        borderColor:'red',
+      }
+      ]} variant="contained">Button</Button>
     </div>
   );
 }
