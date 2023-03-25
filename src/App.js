6cf15a564491854d2a53d2e2a84759bf78f3380b
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-03-25 16:18:00 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>React Mui5</Typography>
+    </div>
+  );
+}
+
+export default App;
