bc4b6fbd7acb7f3e978f5029e2e7a9fc412080db
fhabhishektiwari <abhishektiwarifh@gmail.com>
2023-03-25 16:27:22 +0530

    added theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import {ThemeProvider,createTheme} from '@mui/material';
+
+const theme=createTheme({
+  palette:{
+    primary:{
+      main:'#f00',
+    },
+    secondary:{
+      main:'#efd',
+    }
+  },
+  typography:{
+    myvariant:{
+      fontSize:21,
+      color:'#f0d',
+    }
+  }
+
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <Typography>React Mui5</Typography>
+        <Typography component="div" variant='myvariant'>React Mui5</Typography>
+      <Button color='primary'>Button</Button>
+      <Button color='secondary' variant='contained'>Button</Button>
     </div>
+    </ThemeProvider>
   );
 }
 
